e8b4d7ce898cd49e0ea6aadc649c5505b616bfb2
serelepadas <gustavo.teixeira1772@gmail.com>
2024-10-01 22:20:47 -0300

    Aula
    doc em C, (bug)

---
+++ novo 1.c
@@ -0,0 +1,62 @@
+RHUA1 - Recursos Humanos em Tecnologia da Informação
+
+
+CRA - CONSELHO REGIONAL DE ADMINISTRAÇÃO + IFSP SPO
+
+CRASP.GOV
+
+ad + minister - direção subordinação
+
+.Alguém que presta serviço para outrém
+.Organização de pessoas para atingir algum resultado
+	.conjunto de pessoas e recursos em busca de lucro e resultados
+
+.Instituição
+
+É desejável que uma empresa seja eficaz e eficiente
+
+Eficiência = Recursos
+Eficácia = objetivos
+Efetividade = impactos
+
+-> entender a difirença entre esses 3 termos (prova)
+
+Funções Administrativas 
+
+PODC
+
+Planejar, Organizar, Dirigir, COntrolar o uso de recursos a fim de alcançar objetivos organacionais.
+
+
+Unidades de depart.
+
+Produção ou Operação
+Marketing e Comercial
+Finanças
+Gestão de Pessoas
+
+adm - tomar decisões
+
+
+áreas funcionais x funções da administração
+
+
+papéis gerenciais
+
+interpessoais
+de informação
+de decisão
+
+
+Administração de empresas
+
+
+dia 22/10 e 29/10 -> sem aula na terça
+
+prestar atenção nos conceitos
+-> vão permear na prova.
+	
+	
+	
+	
+	
